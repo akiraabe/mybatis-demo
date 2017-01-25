b79b1b26dd0325138c7c47e555e7810c4237310a
akiraabe <akiraabe2005@yahoo.co.jp>
2017-01-26 05:39:33 +0900

    Modify data.sql(add some data).

--- mybatis-demo/src/main/resources/data.sql
+++ mybatis-demo/src/main/resources/data.sql
@@ -8,9 +8,13 @@ insert into customers values(2,'Foo Bar','Tokyo','0120-0000-0000');
 -- orders
 insert into orders values(1, '2017-01-22', 1);
 insert into orders values(2, '2017-01-24', 2);
+insert into orders values(3, '2017-01-25', 1);
 -- order_details
 insert into order_details values(1,1,1,100);
 insert into order_details values(2,1,2,50);
 --
 insert into order_details values(3,2,1,120);
 insert into order_details values(4,2,3,50);
+--
+insert into order_details values(5,3,3,20);
+insert into order_details values(6,3,2,10);
